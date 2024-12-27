810ee6e2775c68293549850b7e4e9e6443b74f4b
Boris Nazaroff <7961527+BorisNA@users.noreply.github.com>
2024-12-27 15:49:24 +0300

    Load old config (from the original addon)
    Solution for installing in separate plugin directory

--- chinese/config.py
+++ chinese/config.py
@@ -21,14 +21,32 @@
 from collections import defaultdict
 from json import dump, load
 from os.path import dirname, exists, join, realpath
+from pathlib import Path
 
 from aqt import mw
+from aqt.utils import showInfo
 
 
 class ConfigManager:
+    # Path of the old 'Chinese Support 3' user config
+    _saved_path = Path(mw.pm.addonFolder()).resolve(strict=True) / '1752008591/config_saved.json'
+
     def __init__(self):
         self._load_config()
         mw.addonManager.setConfigUpdatedAction(__name__, lambda *_: self._config_updated_handler())
+
+        if self.config['firstRun'] and self._saved_path.is_file():
+            # Old MacDonald had a farm. E-I-E-I-O.
+            with open(self._saved_path, 'r', encoding='utf-8') as f:
+                config_saved = defaultdict(str, load(f))
+                for k in ['enabledModels', 'speech', 'target', 'max_examples', 'fields']:
+                    self.config[k] = config_saved[k]
+            showInfo( 'Configuration from the old "Chinese Support 3" was imported.\n\n'
+                      'This is a one-time post install action, if you update the configuration in the original addon, '
+                      'it will not be synchronized.\n\n'
+                      'It is recommended to disable the old addon.')
+            self.save()
+
 
     def _load_config(self):
         self.config = mw.addonManager.getConfig(__name__)
